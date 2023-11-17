fa95e8bbe8f82179244cfec71377a127c064fcb2
hardcodedjoy <no-reply@hardcodedjoy.com>
2023-11-17 17:38:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MIC Test (android app)
+
+<code>com.hardcodedjoy.example.mictest</code> <code>v1.0.0</code><br/>
+minSdkVersion: <code>21</code><br/>
+targetSdkVersion: <code>33</code><br/>
+
+## Short description
+
+Simple app that opens the MIC and plots the signal (sound wave).
+
+
+## Description
+
+This app displays the sound wave for the signal received from the device's microphone. It opens the MIC in RAW (UNPROCESSED) stereo mode (CHANNEL_IN_STEREO). In the app settings, the user can modify the Sample Rate (default = 48000 Hz) and select app theme (light / dark / system).
+
+
+## Links
+
+developer website: [https://hardcodedjoy.com](https://hardcodedjoy.com)<br/>
+app page: [https://hardcodedjoy.com/app-details?id=com.hardcodedjoy.example.mictest](https://hardcodedjoy.com/app-details?id=com.hardcodedjoy.example.mictest)<br/>
---
+++ app/src/main/java/com/hardcodedjoy/util/ThemeUtil.java
@@ -0,0 +1,58 @@
+/*
+
+MIT License
+
+Copyright © 2023 HARDCODED JOY S.R.L. (https://hardcodedjoy.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+package com.hardcodedjoy.util;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+
+import com.hardcodedjoy.example.mictest.Keys;
+import com.hardcodedjoy.example.mictest.R;
+
+public class ThemeUtil {
+
+    static public void set(Activity activity, String theme) {
+        switch (theme) {
+            case Keys.light:
+                activity.setTheme(R.style.AppThemeLight);
+                break;
+            case Keys.dark:
+                activity.setTheme(R.style.AppThemeDark);
+                break;
+            case Keys.system:
+                int systemUiMode = activity.getResources().getConfiguration().uiMode;
+                int nightMode = systemUiMode & Configuration.UI_MODE_NIGHT_MASK;
+                if(nightMode == Configuration.UI_MODE_NIGHT_YES) {
+                    activity.setTheme(R.style.AppThemeDark);
+                } else {
+                    activity.setTheme(R.style.AppThemeLight);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
